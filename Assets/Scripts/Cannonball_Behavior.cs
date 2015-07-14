e6b06152e1df0e9a4930e15f822dd44924fe52eb
Emerson Wang <ew4405@hotmail.com>
2015-07-13 22:04:06 -0400

    Placed new Blender cannon as well as cleaned up cannonball behavior when interacting with collisions

---
+++ Assets/Scripts/Cannonball_Behavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannonball_Behavior : MonoBehaviour {
+	
+	//  Behavior of cannonball when colliding with ground or enemy
+	void OnCollisionEnter(Collision impact)
+	{
+		if(impact.gameObject.CompareTag("Surface") || impact.gameObject.CompareTag ("Enemy"))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
