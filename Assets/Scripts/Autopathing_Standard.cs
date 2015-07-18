8f4266b22a66f46c5fa54333ede13c16abbdcfea
Emerson Wang <ew4405@hotmail.com>
2015-07-18 15:42:05 -0400

    Created AOE and Damage system for projectiles, as well as HP system for enemies. Also imported first working model with animations

--- Assets/Scripts/Autopathing_Standard.cs
+++ Assets/Scripts/Autopathing_Standard.cs
@@ -3,15 +3,36 @@ using System.Collections;
 
 public class Autopathing_Standard : MonoBehaviour {
 
-	public Transform wall;
+	private Transform wallPlace;
 	private Vector3 newDestination;
+	private GameObject wall;
+	public float HP;
+	public float DMG;
+
+	void ApplyDamage(float DMGTaken)
+	{
+		HP -= DMGTaken;
+	}
 
 	// Use this for initialization
 	void Start () 
 	{
+		wall = GameObject.Find ("enemyDestination");
+		wallPlace = wall.GetComponent<Transform>();
 		NavMeshAgent enemy = GetComponent<NavMeshAgent>();
-		newDestination = new Vector3(wall.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		newDestination = new Vector3(wallPlace.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 		enemy.destination = newDestination;
+	}
+
+	void Update()
+	{
+		NavMeshAgent enemy = GetComponent<NavMeshAgent>();
+		newDestination = new Vector3(wallPlace.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		enemy.destination = newDestination;
+		if(HP <= 0)
+		{
+			Destroy(gameObject);
+		}
 	}
 
 }
--- Assets/Scripts/Cannonball_Behavior.cs
+++ Assets/Scripts/Cannonball_Behavior.cs
@@ -2,12 +2,29 @@
 using System.Collections;
 
 public class Cannonball_Behavior : MonoBehaviour {
-	
+
+	public int AOE;
+	public float DMG;
+
 	//  Behavior of cannonball when colliding with ground or enemy
 	void OnCollisionEnter(Collision impact)
 	{
+
+		// Explodes cannonball upon impact with the ground or an enemy
 		if(impact.gameObject.CompareTag("Surface") || impact.gameObject.CompareTag ("Enemy"))
 		{
+			//Checking if the cannonball AOE hits an enemy.
+			Collider[] AOEcheck = Physics.OverlapSphere(impact.transform.position, AOE);
+			foreach(Collider other in AOEcheck)
+			{
+				GameObject enemy = other.gameObject;
+				if(enemy.gameObject.CompareTag("Enemy"))
+				{
+					Debug.Log("Hit");
+					enemy.SendMessage ("ApplyDamage", DMG, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+				
 			Destroy(gameObject);
 		}
 	}
